b37860bf3a15d55d2daefda10bbbf861cdc3ef5b
elkuku <der.el.kuku@gmail.com>
2023-12-02 14:45:23 +0100

    Reorder

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,7 @@
-import './bootstrap.js';
 // Bootstrap Stimulus
-
-// Color theme switcher
-import './lib/twbs/color-modes.js'
-
-import './styles/app.css'
+import './bootstrap.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+
+import './styles/app.css'
