fa7cfda5752412aed1586d139867022fa118e299
elkuku <der.el.kuku@gmail.com>
2024-07-10 11:16:01 -0500

    Add ux icons

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
