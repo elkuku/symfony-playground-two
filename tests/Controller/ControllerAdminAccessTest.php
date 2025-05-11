dd3f99b8042b0e8df8b7610332cec23fcff737bf
elkuku <der.el.kuku@gmail.com>
2025-05-11 13:32:13 -0500

    Update tests

--- tests/Controller/ControllerAdminAccessTest.php
+++ tests/Controller/ControllerAdminAccessTest.php
@@ -20,10 +20,10 @@ class ControllerAdminAccessTest extends ControllerBaseTest
             'login' => [
                 'statusCodes' => ['GET' => 200],
             ],
-            /*
             'app_system_logview' => [
                 'statusCodes' => ['GET' => 200],
             ],
+            /*
             'app_system_about' => [
                 'statusCodes' => ['GET' => 200],
             ],
